232119b1a978cb6bd02a1b57128bbfaacc9a3232
ROXANA lazarescu <roxana_lazarescu13@yahoo.com>
2022-05-01 23:23:44 +0300

    Add project files.

---
+++ hELLOwORLD10.1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace hELLOwORLD10._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //suma cifrelor unui numar intreg
+            Console.WriteLine("Introduceti numarul");
+            int x = int.Parse(Console.ReadLine());
+            int SumaCifrelor = x / 10 + x % 10;
+            Console.WriteLine("Suma cifrelor este" + SumaCifrelor); 
+        }
+    }
+}
